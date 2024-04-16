1decd6ab42e78201f715eb020af8b3224cc1869e
Shreyak Godala <shreyakgodala2909@gmail.com>
2024-04-16 19:43:39 -0400

    Initial Commit

---
+++ FetchiOSTakeHome/FetchiOSTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchiOSTakeHomeApp.swift
+//  FetchiOSTakeHome
+//
+//  Created by Shreyak Godala on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchiOSTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
